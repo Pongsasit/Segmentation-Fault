0bc333287a2f7bc64cd72ab923020d6536d8fdfc
Juierror <siwa.boonpun@gmail.com>
2022-06-19 08:10:07 +0700

    chore: update data prepare logic

--- src/data/torch_data.py
+++ src/data/torch_data.py
@@ -3,10 +3,12 @@ import cv2
 import glob
 import numpy as np
 from tqdm import tqdm
+from multiprocessing.pool import ThreadPool
 from torch.utils.data import DataLoader, Dataset
 
 class CustomDataset(Dataset):
-    def __init__(self, data_root="", image_size=100, in_memory=True, feature_list = ["AOT", "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12", "SCL", "TCI", "WVP"]):
+    # ["AOT", "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B11", "B12", "SCL", "TCI", "WVP"]
+    def __init__(self, data_root="", image_size=100, in_memory=True, feature_list = ["B02", "B03", "B04", "B08", "B8A"]):
         self.data_root = data_root
         self.date_list = os.listdir(os.path.join(self.data_root, "2021"))
         self.date_list.sort()
@@ -25,27 +27,55 @@ class CustomDataset(Dataset):
         self.labels = []
 
         if in_memory:
+            num_workers = 4
+            with ThreadPool(num_workers) as pool:
+                tmp_data = list(tqdm(pool.imap(self.get_feature, [i for i in range(self.train_size)]), total=self.train_size))
+
+            for i in tmp_data:
+                self.data.append(tmp_data[i][0])
+                self.labels.append(tmp_data[i][1])
+
             # prepare data
-            for i in tqdm(range(self.train_size)):
-                # 11, 8 come from cv2 imread size of tif
-                output_np = np.zeros((len(self.feature_list), len(self.date_list), self.image_size, self.image_size))
-                label = 0
-                for j in range(len(self.date_list)):
-                    for k in range(len(self.feature_list)):
-                        date = self.date_list[j]
-                        feature = self.feature_list[k]
-                        img_root_path = os.path.join(self.data_root, "2021", date, i)
-                        image_name = "*_47PQS_{}_{}.tif".format(date, feature)
-                        img_path = glob(os.path.join(img_root_path, image_name))[0]
-                        label = img_path.split("/")[-1][0]
-                        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                        img = self.image_resize(img, desired_size=self.image_size)
-                        output_np[j, k, :, :] = img
-                self.data.append(output_np)
-                self.labels.append(self.label_map[label])
+            # for i in tqdm(range(self.train_size)):
+            #     output_np, label = self.get_feature(i)
+            #     self.data.append(output_np)
+            #     self.labels.append(label)
 
     def __len__(self):
         return self.train_size
+
+    def get_feature(self, i):
+        # read cache
+        if os.path.exists(os.path.join(self.data_root, "npy", str(i)+".npy")):
+            with open(os.path.join(self.data_root, "npy", str(i)+".npy"), "rb") as f:
+                output_np = np.load(f)
+                img_root_path = os.path.join(self.data_root, "2021", self.date_list[0], str(i))
+                image_name = "*_47PQS_{}_{}.tif".format(self.date_list[0], self.feature_list[0])
+                img_path = glob.glob(os.path.join(img_root_path, image_name))[0]
+                label = img_path.split("/")[-1][0]
+                return output_np, self.label_map[label]
+
+        output_np = np.zeros((len(self.feature_list), len(self.date_list), self.image_size, self.image_size))
+        label = 0
+        for j in range(len(self.date_list)):
+            for k in range(len(self.feature_list)):
+                date = self.date_list[j]
+                feature = self.feature_list[k]
+                img_root_path = os.path.join(self.data_root, "2021", date, str(i))
+                image_name = "*_47PQS_{}_{}.tif".format(date, feature)
+                img_path = glob.glob(os.path.join(img_root_path, image_name))[0]
+                label = img_path.split("/")[-1][0]
+                img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                img = self.image_resize(img, desired_size=self.image_size)
+                output_np[k, j, :, :] = img
+
+        # write cache
+        if not os.path.exists(os.path.join(self.data_root, "npy")):
+            os.makedirs(os.path.join(self.data_root, "npy"))
+        with open(os.path.join(self.data_root, "npy", str(i)+".npy"), "wb") as f:
+            np.save(f, output_np)
+
+        return output_np, self.label_map[label]
 
     def image_resize(self, im, desired_size=100):
         old_size = im.shape[:2] # old_size is in (height, width) format
@@ -72,34 +102,9 @@ class CustomDataset(Dataset):
         if self.in_memory:
             return self.data[idx]
         else:
-            # read cache
-            if os.path.exists(os.path.join(self.data_root, "2021", idx+".npy")):
-                with open(os.path.join(self.data_root, "2021", idx+".npy"), "rb") as f:
-                    output_np = np.load(f)
-                    img_root_path = os.path.join(self.data_root, "2021", self.date_list[0], idx)
-                    image_name = "*_47PQS_{}_{}.tif".format(self.date_list[0], self.feature_list[0])
-                    img_path = glob(os.path.join(img_root_path, image_name))[0]
-                    label = img_path.split("/")[-1][0]
-                    return output_np, label
+            output_np, label = self.get_feature(idx)
 
-            output_np = np.zeros((len(self.feature_list), len(self.date_list), 100, 100))
-            label = 0
-            for j in range(len(self.date_list)):
-                for k in range(len(self.feature_list)):
-                    date = self.date_list[j]
-                    feature = self.feature_list[k]
-                    img_root_path = os.path.join(self.data_root, "2021", date, idx)
-                    image_name = "*_47PQS_{}_{}.tif".format(date, feature)
-                    img_path = glob(os.path.join(img_root_path, image_name))[0]
-                    label = img_path.split("/")[-1][0]
-                    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                    img = self.image_resize(img, desired_size=self.image_size)
-                    output_np[j, k, :, :] = img
-            # write cache
-            with open(os.path.join(self.data_root, "2021", idx+".npy"), "rb") as f:
-                np.save(f, output_np)
-
-            return output_np, self.label_map[label]
+            return output_np, label
 
 def get_train_data(batch_size: int = 32, data_root="", image_size=100, in_memory=True):
     """
